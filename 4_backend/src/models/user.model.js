2fd3afe770bb8906ce72b03617852cdde1342100
ashish-509 <101721265+ashish-509@users.noreply.github.com>
2024-06-09 09:08:03 +0545

    user model and video model partial work done

---
+++ 4_backend/src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // this helps in optimize and proper searching in database although it becomes little expensive.
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // here, we get the url of image in String which is stored in Cloudinary.
+      required: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
