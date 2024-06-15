5a49a43b90d6ea270abe9c36226f204ce5164e27
ashish-509 <101721265+ashish-509@users.noreply.github.com>
2024-06-15 14:58:07 +0545

    accessRefreshToken middlewares and cookies set

--- 4_backend/src/controllers/user.controller.js
+++ 4_backend/src/controllers/user.controller.js
@@ -1,92 +1,200 @@
-import { asyncHandler } from '../utils/asyncHandler.js'
-import { ApiError } from '../utils/ApiError.js'
-import { User } from '../models/user.model.js '
-import { uploadOnCloudinary } from '../utils/cloudinary.js'
-import { ApiResponse   } from '../utils/ApiResponse.js'
- 
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js ";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler (async (req, res) => {
 
-    // Algorithm :
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        
+        user.refreshToken = refreshToken
+        user.save( { validateBeforeSave : false } )
 
-        // get user details from frontend
+        return { accessToken, refreshToken }
 
-        // validation - check if any of fields are empty
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access tokens.")
+    }
+}
 
-        // check if user already exits - username, email
 
-        // check for images and avatar
+const registerUser = asyncHandler(async (req, res) => {
+  // Algorithm :
 
-        // upload them to cloudinary
+  // get user details from frontend
 
-        // create user object - create entry in db
+  // validation - check if any of fields are empty
 
-        // remove password and refresh token from response
+  // check if user already exits - username, email
 
-        // check for user creation
+  // check for images and avatar
 
-        // return response
+  // upload them to cloudinary
 
-    
-    // from ra json dekhi aako data req.body() ma pauxa tara url bata aako data paudaina
-    const {fullName, email, username, password} = req.body
+  // create user object - create entry in db
 
-    console.log('email : ', email)
+  // remove password and refresh token from response
 
-    if (
-        [fullName, email, username, password].some((field) => field?.trim() === "")
-    ){
-        throw new ApiError (400, "ALl fields are required.")
+  // check for user creation
+
+  // return response
+
+  // from ra json dekhi aako data req.body() ma pauxa tara url bata aako data paudaina
+  const { fullName, email, username, password } = req.body;
+
+  console.log("email : ", email);
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "ALl fields are required.");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User with same username or email already exits.");
+  }
+
+  // req.body ko access express le by default dinxa vane req.files ko access multer le dinxa.
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar Image is required.");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar Image is required.");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "", // As cover image is not compulsory... code fatna bata jogauna ... make it empty if not provided by user.
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshYToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user.");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully !"));
+
+  const loginUser = asyncHandler(async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookie
+
+    const { email, username, password } = req.body;
+
+    if (!username || !email) {
+      throw new ApiError(400, "Username or email is required.");
     }
 
-    const existedUser = await User.findOne({
-        $or : [{ username }, { email }]
-    }) 
-    if (existedUser) {
-        throw new ApiError (409 , "User with same username or email already exits.")
+    const user = await User.findOne({
+      $or: [{ username, email }],
+    });
+
+    if (!user) {
+      throw new ApiError(404, "User does not exist.");
     }
 
-    // req.body ko access express le by default dinxa vane req.files ko access multer le dinxa.
-    const avatarLocalPath = req.files?.avatar[0]?.path  
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    const isPasswordValid = await user.isPasswordCorrect(password)
 
-    let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path
+    if ( !isPasswordValid ) {
+        throw new ApiError ( 401, "Invalid user credentials." )
     }
 
-    if ( ! avatarLocalPath ){
-        throw new ApiError (400, "Avatar Image is required.")
-    }
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
 
-    const avatar = await uploadOnCloudinary (avatarLocalPath)
-    const coverImage = await uploadOnCloudinary (coverImageLocalPath)
+    const loggedInUser = await User.findById(user._id).select(' -refreshToken -password ')
 
-    if (!avatar) {
-        throw new ApiError (400, "Avatar Image is required.")
-    }
 
-    const user = await User.create({
-        fullName,
-        avatar : avatar.url,
-        coverImage : coverImage?.url || "", // As cover image is not compulsory... code fatna bata jogauna ... make it empty if not provided by user.
-        email,
-        password,
-        username : username.toLowerCase()
-    })
+    // Send cookie
 
-    const createdUser = await User.findById(user._id).select("-password -refreshYToken")
+    // By default cookies are modifiable from front-end as well.
 
-    if (!createdUser) {
-        throw new ApiError (500, "Something went wrong while registering the user.")
-    }
+    const options = { // but this makes it modifiable from server only
+        httpOnly : true,
+        secure : true
+    } 
 
-    return res.status(201).json(
-        new ApiResponse (200, createdUser, "User registered successfully !")
+    return res
+    .status(200)
+    .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user : loggedInUser, accessToken, refreshToken
+            },
+            "User logged in Successfully."
+        )
     )
 
+  }); 
+});
 
-})
 
+// LogOut...
 
-export { registerUser }
+ const logoutUser = asyncHandler (async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set : {
+                refreshToken : undefined
+            }
+        },
+        {
+            new : true  
+        }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse (200, {}, "User logged out successfully."))
+ })
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+};
